51b333551d10d17ae832d248a834bf39ae8ee080
parkjaehyoung <kisspa@hanmail.net>
2020-01-31 23:27:18 +0900

    spring core di
    (cherry picked from commit ef8645e7deabb5053eb1fb7b6181d435f84e6008)

--- src/main/java/com/tistory/kisspa/comp/diforxml/SampleForDiXml.java
+++ src/main/java/com/tistory/kisspa/comp/diforxml/SampleForDiXml.java
@@ -3,7 +3,7 @@ package com.tistory.kisspa.comp.diforxml;
 import com.tistory.kisspa.comp.SampleComponent;
 
 public class SampleForDiXml implements SampleComponent {
-    @Override
+
     public String saySomething() {
         return "hello, i'm SampleComponent DI Xml";
     }
